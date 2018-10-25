0e0004b278844323c60fd74b53efb578060a6e93
Igor Palyvoda <tib.igor.mk@gmail.com>
2018-10-25 13:07:53 +0300

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by user on 10/25/18.
+//  Copyright © 2018 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
