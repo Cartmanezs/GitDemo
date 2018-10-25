0874e9398247aff0c9f5c8e83ab39577a9a894ee
Igor Palyvoda <tib.igor.mk@gmail.com>
2018-10-25 13:18:15 +0300

    Second commit with modified appdelegate with print statement

--- GitDemo/AppDelegate.swift
+++ GitDemo/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+
+
+        print("Hello")
+
         return true
     }
 
